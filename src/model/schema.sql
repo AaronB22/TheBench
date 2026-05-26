e18a8da37addd8450581a4e0b6e5dca38e5354f0
AaronB22 <aaronthesnowboarder@live.com>
2026-05-26 13:53:06 -0700

    added schema file

---
+++ src/model/schema.sql
@@ -0,0 +1,24 @@
+create database bench_dev;
+
+use bench_dev;
+
+CREATE TABLE user (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(15),
+    password VARCHAR(20)
+);
+use bench_dev;
+CREATE TABLE orders (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    dateTime VARCHAR(200),
+    adress VARCHAR(200),
+    user_id INT,
+    FOREIGN KEY (user_id) REFERENCES users(id)
+);
+use bench_dev;
+CREATE TABLE products (
+    ProductID int PRIMARY KEY,
+    name varchar(255),
+    price decimal(10, 2),
+    type varchar(255)
+);
