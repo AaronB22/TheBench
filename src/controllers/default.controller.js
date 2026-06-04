e3dc3b12cea8bffc32933c42227de38212d32900
AaronB22 <aaronthesnowboarder@live.com>
2026-06-04 12:12:52 -0700

    added controller outline

--- src/controllers/default.controller.js
+++ src/controllers/default.controller.js
@@ -1,1 +1,28 @@
-//place controller functions here...
+
+
+export const getAllProducts=(res,req)=>{
+    try{
+
+    }
+    catch{
+        res.status(500).json({message:'Server failed :('})
+    }
+}
+
+export const getProduct=(req,res)=>{
+    try{
+        const id= req.params.id;
+    }
+    catch{
+
+    }
+}
+
+export const logIn=(req,res)=>{
+    try{
+
+    }
+    catch{
+        
+    }
+}
--- src/routers/default.routes.js
+++ src/routers/default.routes.js
@@ -5,5 +5,8 @@ const router = Router();
 router.get("/", (req, res) => {
     res.render("home");
 });
+//get all products
+router.get('/products');
 
+router.get('/product:id');
 export default router;
