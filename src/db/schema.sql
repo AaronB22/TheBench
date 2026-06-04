9da75e9404798a369f36643844440c5ec734100d
AaronB22 <aaronthesnowboarder@live.com>
2026-06-04 13:05:29 -0700

    fixed schema wording

--- src/db/schema.sql
+++ src/db/schema.sql
@@ -3,7 +3,7 @@ create database bench_dev;
 
 use bench_dev;
 
-CREATE TABLE user (
+CREATE TABLE users (
     id INT AUTO_INCREMENT PRIMARY KEY,
     username VARCHAR(15),
     password VARCHAR(20)
@@ -12,9 +12,9 @@ CREATE TABLE user (
 CREATE TABLE orders (
     id INT AUTO_INCREMENT PRIMARY KEY,
     dateTime VARCHAR(200),
-    adress VARCHAR(200),
+    address VARCHAR(200),
     user_id INT,
-    FOREIGN KEY (user_id) REFERENCES user(id)
+    FOREIGN KEY (user_id) REFERENCES users(id)
 );
 
 CREATE TABLE products (
