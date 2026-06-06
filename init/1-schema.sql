13a4e3ae8bc910a2b445c1472bd331cfd1e8f628
JHud <219741840+JamHudson@users.noreply.github.com>
2026-06-06 12:06:42 -0700

    Set up compose file to create, seed, and grant privileges on first compose

---
+++ init/1-schema.sql
@@ -0,0 +1,23 @@
+CREATE DATABASE IF NOT EXISTS bench_dev;
+use bench_dev;
+
+CREATE TABLE IF NOT EXISTS users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(15),
+    password VARCHAR(20)
+);
+
+CREATE TABLE IF NOT EXISTS orders (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    dateTime VARCHAR(200),
+    address VARCHAR(200),
+    user_id INT,
+    FOREIGN KEY (user_id) REFERENCES users(id)
+);
+
+CREATE TABLE IF NOT EXISTS products (
+    ProductID int PRIMARY KEY,
+    name varchar(255),
+    price decimal(10, 2),
+    type varchar(255)
+);
