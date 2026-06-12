6a8925aac42189a184a385d7905beb47e3cba8ff
JHud <219741840+JamHudson@users.noreply.github.com>
2026-06-11 21:51:13 -0700

    Controller + route setup

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,20 +1,58 @@
-import { } from '../services/user.service.js';
+import { createUser, findUserByUsername, validatePassword} from '../services/user.service.js';
 
 const register = async (req, res) => {
+    const { username, password, confirm } = req.body;
+    // Validate inputs
+    // All fields required
+    if (!username || !password || !confirm) {
+        return res.redirect("/register");
+    }
+
+    // Passwords dont' match
+    if (password !== confirm) {
+        return res.redirect("/register");
+    }
+
     // Create a new user account
+    await createUser(username, password);
     // Redirect to /login
+    return res.redirect("/login");
 }
 
 const login = async (req, res) => {
-    // Validate credentials
+    const { username, password } = req.body;
+    // Validate Credentials
+    
+    // All fields required
+    if (!username || !password) {
+        res.redirect("/login");
+    }
+
+    const user = await findUserByUsername(username);
+    // Account does not exist
+    if (!user) {
+        return res.redirect("/login");
+    }
+
+    // Invalid password 
+    if (!await validatePassword(password, user.password)) {
+        return res.redirect("/login");
+    }
 
     // Create a new session
+    req.session.user = {
+        userId: user.userId,
+        username: user.username
+    }
+
     // Redirect to /products
+    return res.redirect("/products")
 };
 
 const logout = (req, res) => {
     // Destroy the session
     // Redirect to /login
+    req.session.destroy(() => res.redirect("/login"));
 };
 
 export default { register, login, logout };
--- src/routers/user.routes.js
+++ src/routers/user.routes.js
@@ -1,16 +1,16 @@
 import Router from 'express';
-import { } from '../controllers/user.controllers.js';
+import { register, login, logout } from '../controllers/user.controllers.js';
 
 const router = new Router();
 
 // Register
 router.get("/register");
-router.post("/register", );
+router.post("/register", register);
 
 // Login
 router.get("/login");
-router.post("/login");
+router.post("/login", login);
 
 // Logout
-router.post("/logout");
+router.post("/logout", logout);
 
