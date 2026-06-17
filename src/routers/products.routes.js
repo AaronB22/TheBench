0409788add996538dd3e84c35a5d42f5cd3b2b6b
JHud <219741840+JamHudson@users.noreply.github.com>
2026-06-16 17:58:16 -0700

    Authorization on prodcut and cart pages

--- src/routers/products.routes.js
+++ src/routers/products.routes.js
@@ -3,11 +3,11 @@ import { getAllProducts, getProduct} from '../controllers/products.controller.js
 import { addCart, testCart } from "../controllers/cart.controller.js";
 const router = Router();
 
-router.get("/products",getAllProducts);
-router.get("/products/:id",getProduct);
+router.get("/products", getAllProducts);
+router.get("/products/:id", getProduct);
 
-router.post('/cart',addCart);
-router.get('/cart',(req,res)=>{
+router.post('/cart', addCart);
+router.get('/cart', (req,res)=>{
     res.status(200).json(req.session.cart);
 })
 
