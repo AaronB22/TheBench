7c6f905dce1338ef2ee5232ddee26f762e20bf5e
JHud <219741840+JamHudson@users.noreply.github.com>
2026-06-18 22:32:34 -0700

    Fixed unexpected price filter limits

--- public/js/products.js
+++ public/js/products.js
@@ -80,8 +80,11 @@ filterBtn.addEventListener('click',applyFilters);
 
 const validateInputs=()=>{
     if(minPrice.value<0) minPrice.value=0;
-    if((maxPrice.value<minPrice.value)&&(maxPrice.value&&minPrice.value)) maxPrice.value=Number(minPrice.value)+1;
-    
+    const min = Number(minPrice.value);
+    const max = Number(maxPrice.value);
+    if((max<min)&&(max&&min)) {
+        maxPrice.value=min+1;
+    }
 }
 minPrice.addEventListener('change',validateInputs);
 maxPrice.addEventListener('change',validateInputs);
