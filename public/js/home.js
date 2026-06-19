3cb3cc01bb3338016c952af17e8ffcb6a5802577
AaronB22 <aaronthesnowboarder@live.com>
2026-06-18 23:04:40 -0700

    featured product

--- public/js/home.js
+++ public/js/home.js
@@ -11,11 +11,12 @@ getFeaturedProducts()
 const buildFeatured = (product) => {
     const section = document.getElementById('featured-items');
     section.insertAdjacentHTML('beforeend', `
-        <div class="featured-card">
-            <img alt="${product.name}">
-            <h3>${product.name}</h3>
-            <p>$${product.price}</p>
-            <a href="/products/${product.id}">View Product</a>
-        </div>
+        <a href="/products/${product.id}">
+            <div class="featured-card">
+                <img alt="${product.name}">
+                <h3>${product.name}</h3>
+                <p>$${product.price}</p>
+            </div>
+        </a>
     `);
 }
