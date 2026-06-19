4c3f27d9125b19b7378e33fd295528279233e907
JHud <219741840+JamHudson@users.noreply.github.com>
2026-06-18 22:14:23 -0700

    individual product endpoint now returns a 404 for null products

--- src/controllers/products.controller.js
+++ src/controllers/products.controller.js
@@ -19,24 +19,27 @@ export const getProduct=async(req,res)=>{
     try{
         const id= req.params.id;
         const result= await findProductById(id);
-        res.status(201).json(result);
+        if (!result) {
+            return res.status(404).json({message: `No product with id: ${id}.`})
+        }
+        return res.status(200).json(result);
     }
     catch(err){
         console.log(err)
-        res.status(500).json({ message: 'Server failed :(' })
+        return res.status(500).json({ message: 'Server failed :(' })
     }
 }
 
-export const getProductByPriceRange=async(req,res)=>{
+// export const getProductByPriceRange=async(req,res)=>{
     
-    try{
-        const minPrice= req.params.minPrice;
-        const maxPrice = req.params.maxPrice;
+//     try{
+//         const minPrice= req.params.minPrice;
+//         const maxPrice = req.params.maxPrice;
         
-        const result = await findProductByPrice(minPrice,maxPrice);
-        res.status(200).json(result);
-    }
-    catch{
-        res.status(500).json({ message: 'Server failed :(' })
-    }
-}
+//         const result = await findProductByPrice(minPrice,maxPrice);
+//         res.status(200).json(result);
+//     }
+//     catch{
+//         res.status(500).json({ message: 'Server failed :(' })
+//     }
+// }
